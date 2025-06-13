6abfa1d5c20284b973f021495bee9b6ffe8c6ab0
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-06-13 18:02:50 -0400

    Improve device selection & brightness calculation

--- led_decible.py
+++ led_decible.py
@@ -3,14 +3,19 @@
 import pyaudio
 import numpy as np
 import time
+import math
 from websockets.sync.client import connect
 
-CHUNK = 4096
-RATE = 44100
+CHUNK = 1024
 NUM_LEDS = 200
-PEAK_DB = 100
+
 p = pyaudio.PyAudio()
-stream = p.open( format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=p.get_default_input_device_info()["index"] )
+for i in range( p.get_device_count() ):
+	info = p.get_device_info_by_index( i )
+	print( str( i ) + " - " + info["name"] )
+DEVICE = int( input( "Enter index of device you want to use: " ) )
+RATE = int( p.get_device_info_by_index( DEVICE )["defaultSampleRate"] )
+stream = p.open( format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=DEVICE )
 
 # Decibel counter modified from https://github.com/itisrmk/Python_decibel-counter/blob/master/Db_Counter.py
 def dbCounter():
@@ -28,12 +33,13 @@ with connect( "ws://colorselector.local:81" ) as ws:
 		while True:
 			data = []
 			db = dbCounter()
-			brightness = np.clip( round( 255 * ( db / PEAK_DB ) ), 0, 255 )
+			finalDb = db ** 1.75 * 0.1
+			brightness = np.clip( round( finalDb ), 0, 255 )
 			for i in range( NUM_LEDS ):
 				data.append( i )
 				data.extend( ( brightness, brightness, brightness ) )
 			ws.send( bytearray( data ) )
-			time.sleep( 0.016 )
+			time.sleep( 0.032 )
 	except KeyboardInterrupt:
 		ws.close()
 		print( "Connection closed." )
